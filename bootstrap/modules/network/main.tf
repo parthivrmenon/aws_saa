5f814b102ea03eea75d4595d9cd4b3aa5191cd0a
Parthiv Roshan Menon <parthiv.menon@smarsh.com>
2025-07-13 14:05:09 +0530

    chore: delete bootstrap

--- bootstrap/modules/network/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  tags = {
-    Name      = "saa-vpc"
-    ManagedBy = "terraform"
-  }
-}
-
--- bootstrap/modules/network/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "cidr_block" {
-  description = "CIDR block to configure for VPC."
-  type        = string
-  default     = "172.1.0.0/16"
-}
-
